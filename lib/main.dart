92caa64638d012a83d30bcd9cba85979b85e79b1
Md Ariful Islam <jahorularif@gmail.com>
2021-03-01 20:58:27 +0800

    AppBar with Body

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stupeed',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("This is AppBar")),
+      body: Text("This is Body"),
+    );
+  }
+}
